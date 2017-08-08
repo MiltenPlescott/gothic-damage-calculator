9edf856fae5d51a1808b2785dd57909a68a054e5
Milten Plescott <milten.plescott@gmx.com>
2017-08-08 15:24:19 +0200

    Call commitEdit() for all JSpinners

--- src/gothicdamagecalculator/CalculationInput.java
+++ src/gothicdamagecalculator/CalculationInput.java
@@ -5,6 +5,9 @@
  */
 package gothicdamagecalculator;
 
+import java.text.ParseException;
+import javax.swing.JSpinner;
+
 /**
  *
  * @author Milten Plescott
@@ -40,6 +43,13 @@ public class CalculationInput {
 	private void readData(MyFrame frame) {
 		gameG1 = frame.gui.rbG1.isSelected();
 		
+		commitSpinner(frame.gui.namelessHero.jsStrength);
+		commitSpinner(frame.gui.namelessHero.jsDexterity);
+		commitSpinner(frame.gui.namelessHero.js1HSkill);
+		commitSpinner(frame.gui.namelessHero.js2HSkill);
+		commitSpinner(frame.gui.namelessHero.jsBow);
+		commitSpinner(frame.gui.namelessHero.jsCrossbow);
+
 		strength = (Integer) frame.gui.namelessHero.jsStrength.getValue();
 		dexterity = (Integer) frame.gui.namelessHero.jsDexterity.getValue();
 		skill1H = (Integer) frame.gui.namelessHero.js1HSkill.getValue();
@@ -49,15 +59,33 @@ public class CalculationInput {
 
 		weaponType = frame.gui.weapon.jcomboWeaponType.getSelectedItem().toString();
 		beliar = frame.gui.weapon.jcheckBeliar.isSelected();
-		
+
+		commitSpinner(frame.gui.weapon.jsDamage);
+		commitSpinner(frame.gui.weapon.jsBonusSkill);
+		commitSpinner(frame.gui.weapon.jsLightningDmg);
+		commitSpinner(frame.gui.weapon.jsLightningChance);
+
 		weaponDamage = (Integer) frame.gui.weapon.jsDamage.getValue();
 		skillBonus = (Integer) frame.gui.weapon.jsBonusSkill.getValue();
 		lightningDamage = (Integer) frame.gui.weapon.jsLightningDmg.getValue();
 		lightningChance = (Integer) frame.gui.weapon.jsLightningChance.getValue();
 
+		commitSpinner(frame.gui.target.jsHealth);
+		commitSpinner(frame.gui.target.jsHits);
+		commitSpinner(frame.gui.target.jsMelee);
+		commitSpinner(frame.gui.target.jsRanged);
+
 		health = (Integer) frame.gui.target.jsHealth.getValue();
 		hits = (Integer) frame.gui.target.jsHits.getValue();
 		meleeArmor = (Integer) frame.gui.target.jsMelee.getValue();
 		rangedArmor = (Integer) frame.gui.target.jsRanged.getValue();
+	}
+	
+	private void commitSpinner(JSpinner spinner) {
+		try {
+			spinner.commitEdit();
+		}
+		catch (ParseException e) {
+		}
 	}
 }
